12f0e54c4ee08ef64d19ebe0891aa5f00fe569bb
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-30 20:24:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,32 @@
-# Write your code here!
+require 'pp'
+require 'pry'
 
 
+def game_hash 
+  {:home => 
+     {:team_name => "brooklyn_nets",
+      :colors => [black , white]
+      :players => [
+        {player_name: "Allen Anderson"   , number: 0    , shoe: 16   , points: 22   , rebounds: 12   , assists: 12   , steals: 3 , blocks: 1  , slam_dunks: 1    }
+        
+        {player_name:"Regggie Evans"   , number: 14    , shoe: 14   , points: 12   , rebounds:   , assists:   , steals:   , blocks:  , slam_dunks:     }
+  
+        {player_name:   , number:    , shoe:   , points:   , rebounds:   , assists:   , steals:   , blocks:  , slam_dunks:     }
+         
+        {player_name:   , number:    , shoe:   , points:   , rebounds:   , assists:   , steals:   , blocks:  , slam_dunks:     }
+          
+        {player_name:   , number:    , shoe:   , points:   , rebounds:   , assists:   , steals:   , blocks:  , slam_dunks:     }]
+  
+   
+  
+  
+  :away => {}
+  
+  
 
 
-
-
-
+  }
+ end
 
 
 
